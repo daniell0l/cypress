ba1d40d76d1af1ee1196033b2ba9755a6be1459d
daniell0l <daniel_l0l@hotmail.com>
2022-05-02 18:02:32 -0300

    testando api

---
+++ cypress/integration/ALURAPIC/api-alurapic.spec.js
@@ -0,0 +1,15 @@
+
+describe('Busca fotos e dados', () => {
+
+    it('busca fotos do flavio', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://apialurapic.herokuapp.com/flavio/photos'
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('Farol iluminado')
+        })
+    })
+})
